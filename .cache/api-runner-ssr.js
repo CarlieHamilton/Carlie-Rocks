9b2b8fe8f422767ed2634ebd08d5b6fd06583c35
BlueCodeThree <carliemhamilton@gmail.com>
2020-05-06 19:07:05 +1000

    remove node_modules

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,8 +1,8 @@
 var plugins = [{
-      plugin: require('/home/carlie/Personal/Carlie-Rocks/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
+      plugin: require('/home/carlie/personal/Carlie-Rocks/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     },{
-      plugin: require('/home/carlie/Personal/Carlie-Rocks/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
+      plugin: require('/home/carlie/personal/Carlie-Rocks/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/gatsby-icon.png"},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,9 +5,9 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/carlie/Personal/Carlie-Rocks/.cache/dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/home/carlie/Personal/Carlie-Rocks/src/pages/404.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/home/carlie/Personal/Carlie-Rocks/src/pages/index.js"))),
-  "component---src-pages-page-2-js": hot(preferDefault(require("/home/carlie/Personal/Carlie-Rocks/src/pages/page-2.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/carlie/personal/Carlie-Rocks/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/home/carlie/personal/Carlie-Rocks/src/pages/404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/home/carlie/personal/Carlie-Rocks/src/pages/index.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("/home/carlie/personal/Carlie-Rocks/src/pages/page-2.js")))
 }
 
